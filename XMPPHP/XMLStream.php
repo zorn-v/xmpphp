eb08b045c3b00ea77e4f5098468af3ed57a8f723
Ivan Borzenkov <ivan1986@list.ru>
2010-03-10 00:01:19 +0300

    Return stream_blocking and add false check

--- XMPPHP/XMLStream.php
+++ XMPPHP/XMLStream.php
@@ -457,8 +457,15 @@ class XMPPHP_XMLStream {
 			} else if ($updated > 0) {
 				$buff = '';
 				do {
+					if ($buff != '') {
+						//disable blocking for now because fread() will
+						// block until the 4k are full if we already
+						// read a part of the packet
+						stream_set_blocking($this->socket, 0);
+					}
 					$part = fread($this->socket, 4096);
-					if (!$part) {
+					stream_set_blocking($this->socket, 1);
+					if ($part === false) {
 						if($this->reconnect) {
 							$this->doReconnect();
 						} else {
