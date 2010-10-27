430762042cefc3e0102925e026e241b4344c6781
Ivan Borzenkov <ivan1986@list.ru>
2010-10-27 16:00:19 +0800

    fix example - notices

--- examples/cli_longrun_example.php
+++ examples/cli_longrun_example.php
@@ -13,38 +13,40 @@ $conn->autoSubscribe();
 $vcard_request = array();
 
 try {
-    $conn->connect();
-    while(!$conn->isDisconnected()) {
-    	$payloads = $conn->processUntil(array('message', 'presence', 'end_stream', 'session_start', 'vcard'));
-    	foreach($payloads as $event) {
-    		$pl = $event[1];
-    		switch($event[0]) {
-    			case 'message': 
-    				print "---------------------------------------------------------------------------------\n";
-    				print "Message from: {$pl['from']}\n";
-    				if($pl['subject']) print "Subject: {$pl['subject']}\n";
-    				print $pl['body'] . "\n";
-    				print "---------------------------------------------------------------------------------\n";
-    				$conn->message($pl['from'], $body="Thanks for sending me \"{$pl['body']}\".", $type=$pl['type']);
+	$conn->connect();
+	while(!$conn->isDisconnected()) {
+		$payloads = $conn->processUntil(array('message', 'presence', 'end_stream', 'session_start', 'vcard'));
+		foreach($payloads as $event) {
+			$pl = $event[1];
+			switch($event[0]) {
+				case 'message': 
+					if (empty($pl['body']))
+						break;
+					print "---------------------------------------------------------------------------------\n";
+					print "Message from: {$pl['from']}\n";
+					if(!empty($pl['subject'])) print "Subject: {$pl['subject']}\n";
+					print $pl['body'] . "\n";
+					print "---------------------------------------------------------------------------------\n";
+					$conn->message($pl['from'], $body="Thanks for sending me \"{$pl['body']}\".", $type=$pl['type']);
 					$cmd = explode(' ', $pl['body']);
-    				if($cmd[0] == 'quit') $conn->disconnect();
-    				if($cmd[0] == 'break') $conn->send("</end>");
-    				if($cmd[0] == 'vcard') {
+					if($cmd[0] == 'quit') $conn->disconnect();
+					if($cmd[0] == 'break') $conn->send("</end>");
+					if($cmd[0] == 'vcard') {
 						if(!($cmd[1])) $cmd[1] = $conn->user . '@' . $conn->server;
 						// take a note which user requested which vcard
 						$vcard_request[$pl['from']] = $cmd[1];
 						// request the vcard
 						$conn->getVCard($cmd[1]);
 					}
-    			break;
-    			case 'presence':
-    				print "Presence: {$pl['from']} [{$pl['show']}] {$pl['status']}\n";
-    			break;
-    			case 'session_start':
-    			    print "Session Start\n";
-			    	$conn->getRoster();
-    				$conn->presence($status="Cheese!");
-    			break;
+				break;
+				case 'presence':
+					print "Presence: {$pl['from']} [{$pl['show']}] {$pl['status']}\n";
+				break;
+				case 'session_start':
+					print "Session Start\n";
+					$conn->getRoster();
+					$conn->presence($status="Cheese!");
+				break;
 				case 'vcard':
 					// check to see who requested this vcard
 					$deliver = array_keys($vcard_request, $pl['from']);
@@ -66,12 +68,12 @@ try {
 					foreach($deliver as $sendjid) {
 						// remove the note on requests as we send out the message
 						unset($vcard_request[$sendjid]);
-    					$conn->message($sendjid, $msg, 'chat');
+						$conn->message($sendjid, $msg, 'chat');
 					}
 				break;
-    		}
-    	}
-    }
+			}
+		}
+	}
 } catch(XMPPHP_Exception $e) {
-    die($e->getMessage());
+	die($e->getMessage());
 }
