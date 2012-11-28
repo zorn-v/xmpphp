26fc0e79c7662b3c19de4c80f314e8eff1c80f38
Aarón Nieves Fernández <tecnocat128@gmail.com>
2012-11-28 18:50:14 +0100

    Minor changes for indentation and spaces

--- XMPPHP/XMLObj.php
+++ XMPPHP/XMLObj.php
@@ -1,77 +1,78 @@
-<?php 
+<?php
+
 /**
  * XMPPHP: The PHP XMPP Library
  * Copyright (C) 2008  Nathanael C. Fritz
  * This file is part of SleekXMPP.
- * 
+ *
  * XMPPHP is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * XMPPHP is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with XMPPHP; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * @category   xmpphp 
- * @package	XMPPHP
- * @author	 Nathanael C. Fritz <JID: fritzy@netflint.net>
- * @author	 Stephan Wentz <JID: stephan@jabber.wentz.it>
- * @author	 Michael Garvin <JID: gar@netflint.net>
- * @copyright  2008 Nathanael C. Fritz
+ * @category  xmpphp
+ * @package   XMPPHP
+ * @author    Nathanael C. Fritz <JID: fritzy@netflint.net>
+ * @author    Stephan Wentz <JID: stephan@jabber.wentz.it>
+ * @author    Michael Garvin <JID: gar@netflint.net>
+ * @copyright 2008 Nathanael C. Fritz
  */
 
 /**
  * XMPPHP XML Object
- * 
- * @category   xmpphp 
- * @package	XMPPHP
- * @author	 Nathanael C. Fritz <JID: fritzy@netflint.net>
- * @author	 Stephan Wentz <JID: stephan@jabber.wentz.it>
- * @author	 Michael Garvin <JID: gar@netflint.net>
- * @copyright  2008 Nathanael C. Fritz
- * @version	$Id$
+ *
+ * @package   XMPPHP
+ * @author    Nathanael C. Fritz <JID: fritzy@netflint.net>
+ * @author    Stephan Wentz <JID: stephan@jabber.wentz.it>
+ * @author    Michael Garvin <JID: gar@netflint.net>
+ * @copyright 2008 Nathanael C. Fritz
+ * @version   $Id$
  */
 class XMPPHP_XMLObj {
-	/**
-	 * Tag name
-	 *
-	 * @var string
-	 */
-	public $name;
-	
-	/**
-	 * Namespace
-	 *
-	 * @var string
-	 */
-	public $ns;
-	
-	/**
-	 * Attributes
-	 *
-	 * @var array
-	 */
-	public $attrs = array();
-	
-	/**
-	 * Subs?
-	 *
-	 * @var array
-	 */
-	public $subs = array();
-	
-	/**
-	 * Node data
-	 * 
-	 * @var string
-	 */
-	public $data = '';
+
+  /**
+   * Tag name
+   *
+   * @var string
+   */
+  public $name;
+
+  /**
+   * Namespace
+   *
+   * @var string
+   */
+  public $ns;
+
+  /**
+   * Attributes
+   *
+   * @var array
+   */
+  public $attrs = array();
+
+  /**
+   * Subs?
+   *
+   * @var array
+   */
+  public $subs = array();
+
+  /**
+   * Node data
+   *
+   * @var string
+   */
+  public $data = '';
 
 	/**
 	 * Constructor
