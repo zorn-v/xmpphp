d6bbd423965d7cad146bec58aa7e585ff297c917
Aarón Nieves Fernández <tecnocat128@gmail.com>
2012-11-28 19:13:57 +0100

    Refactor for cleaning code and indentation

--- XMPPHP/Log.php
+++ XMPPHP/Log.php
@@ -38,83 +38,86 @@
  * @version   $Id$
  */
 class XMPPHP_Log {
-	
-	const LEVEL_ERROR   = 0;
-	const LEVEL_WARNING = 1;
-	const LEVEL_INFO	= 2;
-	const LEVEL_DEBUG   = 3;
-	const LEVEL_VERBOSE = 4;
-	
-	/**
-	 * @var array
-	 */
-	protected $data = array();
 
-	/**
-	 * @var array
-	 */
-	protected $names = array('ERROR', 'WARNING', 'INFO', 'DEBUG', 'VERBOSE');
+  const LEVEL_ERROR   = 0;
+  const LEVEL_WARNING = 1;
+  const LEVEL_INFO    = 2;
+  const LEVEL_DEBUG   = 3;
+  const LEVEL_VERBOSE = 4;
 
-	/**
-	 * @var integer
-	 */
-	protected $runlevel;
+  /**
+   * @var array
+   */
+  protected $data = array();
 
-	/**
-	 * @var boolean
-	 */
-	protected $printout;
+  /**
+   * @var array
+   */
+  protected $names = array('ERROR', 'WARNING', 'INFO', 'DEBUG', 'VERBOSE');
 
-	/**
-	 * Constructor
-	 *
-	 * @param boolean $printout
-	 * @param string  $runlevel
-	 */
-	public function __construct($printout = false, $runlevel = self::LEVEL_INFO) {
-		$this->printout = (boolean)$printout;
-		$this->runlevel = (int)$runlevel;
-	}
+  /**
+   * @var integer
+   */
+  protected $runlevel;
 
-	/**
-	 * Add a message to the log data array
-	 * If printout in this instance is set to true, directly output the message
-	 *
-	 * @param string  $msg
-	 * @param integer $runlevel
-	 */
-	public function log($msg, $runlevel = self::LEVEL_INFO) {
-		$time = time();
-		#$this->data[] = array($this->runlevel, $msg, $time);
-		if($this->printout and $runlevel <= $this->runlevel) {
-			$this->writeLine($msg, $runlevel, $time);
-		}
-	}
+  /**
+   * @var boolean
+   */
+  protected $printout;
 
-	/**
-	 * Output the complete log.
-	 * Log will be cleared if $clear = true
-	 *
-	 * @param boolean $clear
-	 * @param integer $runlevel
-	 */
-	public function printout($clear = true, $runlevel = null) {
-		if($runlevel === null) {
-			$runlevel = $this->runlevel;
-		}
-		foreach($this->data as $data) {
-			if($runlevel <= $data[0]) {
-				$this->writeLine($data[1], $runlevel, $data[2]);
-			}
-		}
-		if($clear) {
-			$this->data = array();
-		}
-	}
-	
-	protected function writeLine($msg, $runlevel, $time) {
-		//echo date('Y-m-d H:i:s', $time)." [".$this->names[$runlevel]."]: ".$msg."\n";
-		echo $time." [".$this->names[$runlevel]."]: ".$msg."\n";
-		flush();
-	}
+  /**
+   * Constructor
+   *
+   * @param boolean $printout
+   * @param string  $runlevel
+   */
+  public function __construct($printout = false, $runlevel = self::LEVEL_INFO) {
+    $this->printout = (boolean) $printout;
+    $this->runlevel = (int) $runlevel;
+  }
+
+  /**
+   * Add a message to the log data array
+   * If printout in this instance is set to true, directly output the message
+   *
+   * @param string  $msg
+   * @param integer $runlevel
+   */
+  public function log($msg, $runlevel = self::LEVEL_INFO) {
+    $time = time();
+    // $this->data[] = array($this->runlevel, $msg, $time);
+    if ($this->printout AND $runlevel <= $this->runlevel) {
+      $this->writeLine($msg, $runlevel, $time);
+    }
+  }
+
+  /**
+   * Output the complete log.
+   * Log will be cleared if $clear = true
+   *
+   * @param boolean $clear
+   * @param integer $runlevel
+   */
+  public function printout($clear = true, $runlevel = null) {
+
+    if ($runlevel === null) {
+      $runlevel = $this->runlevel;
+    }
+
+    foreach ($this->data as $data) {
+      if ($runlevel <= $data[0]) {
+        $this->writeLine($data[1], $runlevel, $data[2]);
+      }
+    }
+
+    if ($clear) {
+      $this->data = array();
+    }
+  }
+
+  protected function writeLine($msg, $runlevel, $time) {
+    $sprintf = "\n[%s] (%s) %s";
+    printf($sprintf, date('Y-m-d H:i:s', $time), $this->names[$runlevel], $msg);
+    flush();
+  }
 }
