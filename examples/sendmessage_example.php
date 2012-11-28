ed4545c03a9484232e592c70cb082edeaf416e9d
Aarón Nieves Fernández <tecnocat128@gmail.com>
2012-11-28 20:49:47 +0100

    Refactor for cleaning code and indentation

--- examples/sendmessage_example.php
+++ examples/sendmessage_example.php
@@ -1,20 +1,48 @@
 <?php
 
-// activate full error reporting
-//error_reporting(E_ALL & E_STRICT);
+/**
+ * @file: XMPPHP Send message example
+ *
+ * @info: If this script doesn't work, are you running 64-bit PHP with < 5.2.6?
+ */
 
-include 'XMPPHP/XMPP.php';
+/**
+ * Activate full error reporting
+ * error_reporting(E_ALL & E_STRICT);
+ *
+ * XMPPHP Log levels:
+ *
+ * LEVEL_ERROR   = 0;
+ * LEVEL_WARNING = 1;
+ * LEVEL_INFO    = 2;
+ * LEVEL_DEBUG   = 3;
+ * LEVEL_VERBOSE = 4;
+ */
 
-#Use XMPPHP_Log::LEVEL_VERBOSE to get more logging for error reports
-#If this doesn't work, are you running 64-bit PHP with < 5.2.6?
-$conn = new XMPPHP_XMPP('talk.google.com', 5222, 'username', 'password', 'xmpphp', 'gmail.com', $printlog=false, $loglevel=XMPPHP_Log::LEVEL_INFO);
+require 'XMPPHP/XMPP.php';
+
+$conf = array(
+  'server'   => 'talk.google.com',
+  'port'     => 5222,
+  'username' => 'username',
+  'password' => 'password',
+  'proto'    => 'xmpphp',
+  'domain'   => 'gmail.com',
+  'printlog' => true,
+  'loglevel' => XMPPHP_Log::LEVEL_VERBOSE,
+);
+
+// Easy and simple for access to variables with their names
+extract($conf);
+
+$conn = new XMPPHP_XMPP($server, $port, $username, $password, $proto, $domain, $printlog, $loglevel);
 
 try {
-    $conn->connect();
-    $conn->processUntil('session_start');
-    $conn->presence();
-    $conn->message('someguy@someserver.net', 'This is a test message!');
-    $conn->disconnect();
+  $conn->connect();
+  $conn->processUntil('session_start');
+  $conn->presence();
+  $conn->message('someguy@someserver.net', 'This is a test message!');
+  $conn->disconnect();
 } catch(XMPPHP_Exception $e) {
-    die($e->getMessage());
+  die($e->getMessage());
 }
