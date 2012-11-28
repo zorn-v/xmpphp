f1e2c4c662c1c39dc0e597ac61c3dec2d738a1ea
Aarón Nieves Fernández <tecnocat128@gmail.com>
2012-11-28 18:28:08 +0100

    Minor changes for indentation and spaces

--- XMPPHP/XMLStream.php
+++ XMPPHP/XMLStream.php
@@ -1,39 +1,40 @@
 <?php
+
 /**
  * XMPPHP: The PHP XMPP Library
  * Copyright (C) 2008  Nathanael C. Fritz
  * This file is part of SleekXMPP.
- * 
+ *
  * XMPPHP is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * XMPPHP is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with XMPPHP; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * @category   xmpphp 
- * @package	XMPPHP
- * @author	 Nathanael C. Fritz <JID: fritzy@netflint.net>
- * @author	 Stephan Wentz <JID: stephan@jabber.wentz.it>
- * @author	 Michael Garvin <JID: gar@netflint.net>
- * @copyright  2008 Nathanael C. Fritz
+ * @category  xmpphp
+ * @package   XMPPHP
+ * @author    Nathanael C. Fritz <JID: fritzy@netflint.net>
+ * @author    Stephan Wentz <JID: stephan@jabber.wentz.it>
+ * @author    Michael Garvin <JID: gar@netflint.net>
+ * @copyright 2008 Nathanael C. Fritz
  */
 
 /** XMPPHP_Exception */
-require_once dirname(__FILE__) . '/Exception.php';
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Exception.php';
 
 /** XMPPHP_XMLObj */
-require_once dirname(__FILE__) . '/XMLObj.php';
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'XMLObj.php';
 
 /** XMPPHP_Log */
-require_once dirname(__FILE__) . '/Log.php';
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Log.php';
 
 /**
  * XMPPHP XML Stream
