b0aae5123bbfe4b4112061a77adf35d44606d186
Alexander Birkner <alex.birkner@gmail.com>
2015-01-16 00:08:57 +0100

    # Big refactoring and switched to version 2.0-dev

--- src/BirknerAlex/XMPPHP/XMPP.php
+++ src/BirknerAlex/XMPPHP/XMPP.php
@@ -1,46 +1,45 @@
 <?php
 
-namespace xmpphp;
+namespace BirknerAlex\XMPPHP;
 
-/**
- * XMPPHP: The PHP XMPP Library
- * Copyright (C) 2008  Nathanael C. Fritz
- * This file is part of SleekXMPP.
- * 
- * XMPPHP is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * XMPPHP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with XMPPHP; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * @category   xmpphp 
- * @package	XMPPHP
- * @author	 Nathanael C. Fritz <JID: fritzy@netflint.net>
- * @author	 Stephan Wentz <JID: stephan@jabber.wentz.it>
- * @author	 Michael Garvin <JID: gar@netflint.net>
- * @author	 Alexabder Birkner alex.birkner@©mail.com
- * @copyright  2008 Nathanael C. Fritz
- */
+	/**
+	 * XMPPHP: The PHP XMPP Library
+	 * Copyright (C) 2008  Nathanael C. Fritz
+	 * This file is part of SleekXMPP.
+	 *
+	 * XMPPHP is free software; you can redistribute it and/or modify
+	 * it under the terms of the GNU General Public License as published by
+	 * the Free Software Foundation; either version 2 of the License, or
+	 * (at your option) any later version.
+	 *
+	 * XMPPHP is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 * GNU General Public License for more details.
+	 *
+	 * You should have received a copy of the GNU General Public License
+	 * along with XMPPHP; if not, write to the Free Software
+	 * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	 *
+	 * @category   xmpphp
+	 * @package    XMPPHP
+	 * @author     Nathanael C. Fritz <JID: fritzy@netflint.net>
+	 * @author     Stephan Wentz <JID: stephan@jabber.wentz.it>
+	 * @author     Michael Garvin <JID: gar@netflint.net>
+	 * @author     Alexander Birkner (https://github.com/BirknerAlex)
+	 * @copyright  2008 Nathanael C. Fritz
+	 */
 
 /**
  * XMPPHP Main Class
- * 
- * @category   xmpphp 
- * @package	XMPPHP
- * @author	 Nathanael C. Fritz <JID: fritzy@netflint.net>
- * @author	 Stephan Wentz <JID: stephan@jabber.wentz.it>
- * @author	 Michael Garvin <JID: gar@netflint.net>
- * @author	 Alexabder Birkner alex.birkner@©mail.com
+ *
+ * @category   xmpphp
+ * @package    XMPPHP
+ * @author     Nathanael C. Fritz <JID: fritzy@netflint.net>
+ * @author     Stephan Wentz <JID: stephan@jabber.wentz.it>
+ * @author     Michael Garvin <JID: gar@netflint.net>
  * @copyright  2008 Nathanael C. Fritz
- * @version	$Id$
+ * @version    $Id$
  */
 class XMPP extends XMLStream {
 	/**
@@ -163,11 +162,14 @@ class XMPP extends XMLStream {
 	 * @param string $subject
 	 */
 	public function message($to, $body, $type = 'chat', $subject = null, $payload = null) {
-	    if(is_null($type))
-	    {
+		if ($this->disconnected) {
+			throw new Exception('You need to connect first');
+		}
+
+	    if(empty($type)) {
 	        $type = 'chat';
 	    }
-	    
+
 		$to	  = htmlspecialchars($to);
 		$body	= htmlspecialchars($body);
 		$subject = htmlspecialchars($subject);
@@ -189,6 +191,10 @@ class XMPP extends XMLStream {
 	 * @param string $to
 	 */
 	public function presence($status = null, $show = 'available', $to = null, $type='available', $priority=null) {
+		if ($this->disconnected) {
+			throw new Exception('You need to connect first');
+		}
+
 		if($type == 'available') $type = '';
 		$to	 = htmlspecialchars($to);
 		$status = htmlspecialchars($status);
